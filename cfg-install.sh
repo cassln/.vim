f5d37979331f1d554c87d8a2f099eb0aacb7d4d3
cassln <nikita.mlnk@gmail.com>
2019-05-11 21:38:06 +0200

    add install script

---
+++ cfg-install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+git clone --bare git@github.com:cassln/config.git $HOME/.cfg
+
+function config {
+  git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+config checkout
+if [ $? != 0 ]; then
+  echo "Backing up pre-existing files..."
+  config checkout 2>&1 | egrep "\t+" | awk {'print $1'} | \
+    xargs -I{} sh -c 'mkdir -p "$(dirname .config-backup/{})"; mv {} .config-backup/{}'
+  config checkout
+fi;
+echo "Checked out config."
+
+echo "Initializing submodules..."
+config submodule init
+config submodule update
+echo "Initialized submodules."
+
+config config status.showUntrackedFiles no
+
+function prompt-config-property {
+  local value
+  read -p "Enter git user $1 ($(config config user.$1)): " value
+
+  if [[ ! -z "$value" ]]; then
+    config config user.$1 $value
+  fi
+}
+
+prompt-config-property name
+prompt-config-property email
+
+echo "Done."
